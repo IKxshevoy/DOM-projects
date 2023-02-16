86c80ba87240a20eb8cba582d481e5341ce19d67
Ivan Koshevoy <ivan.koshevoyyy@gmail.com>
2023-02-16 23:57:50 +0000

    added saving to local storage & possibility of adding task on enter click

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,15 +1,25 @@
 const list = document.getElementById('todos');
 const btn = document.querySelector('button');
+const input = document.querySelector('input');
 btn.addEventListener('click', handleclick);
+document.addEventListener('DOMContentLoaded', loadTasks);
+document.addEventListener('keydown', handlekeydown);
 
 function handleclick() {
-    const todo = this.previousElementSibling.value.trim();
+    const todo = input.value.trim();
     if(todo) {
         addTask(todo);
-        this.previousElementSibling.value = "";       
+        saveToStorage(todo);
+        input.value = "";       
     }
     else {
         alert('Please enter a task');
+    }
+}
+
+function handlekeydown(e) {
+    if(e.key === "Enter") {
+        handleclick();
     }
 }
 
@@ -18,9 +28,30 @@ function addTask(text) {
     li.textContent = text;
     list.prepend(li);
     li.addEventListener('click', deleteTask);
+    li.addEventListener('click', deleteFromStorage);
 }
 
 function deleteTask() {
     this.removeEventListener('click', deleteTask);
+    document.removeEventListener('keydown', handlekeydown);
     this.remove();
+}
+
+function saveToStorage(task) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    localStorage.setItem('tasks', JSON.stringify([...tasks, task]));
+}
+
+function deleteFromStorage() {
+    this.removeEventListener('click', deleteFromStorage);
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const newArr = tasks.filter(task => task !== this.textContent);
+    localStorage.setItem('tasks', JSON.stringify(newArr));
+} 
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if(tasks) {
+        tasks.forEach(task => addTask(task));
+    }
 }
